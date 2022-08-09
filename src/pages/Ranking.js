1f7e281d5b3371c0d30a69f6205dff83170c140a
Renato Mendes <mendeslrenato@gmail.com>
2022-08-09 16:58:11 -0300

    requisito 18

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,11 +1,41 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { resetScoreAction } from '../Redux/actions';
+import '../styles/Ranking.css';
 
-export default class Ranking extends Component {
+class Ranking extends Component {
+  handleClick = () => {
+    const { resetScore, history } = this.props;
+    resetScore();
+    history.push('/');
+  }
+
   render() {
     return (
-      <div>
+      <div className="Ranking">
         <h1 data-testid="ranking-title">Ranking</h1>
+        <button
+          data-testid="btn-go-home"
+          type="button"
+          onClick={ this.handleClick }
+        >
+          Play Again
+        </button>
       </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+  resetScore: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  resetScore: () => dispatch(resetScoreAction()),
+});
+
+export default connect(null, mapDispatchToProps)(Ranking);
